8043cf9e251800d0418de638c029c8f6bd3518f3
courtney404570281 <404570281@gapp.fju.edu.tw>
2019-02-24 18:30:22 +0800

    add dataType

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,5 +3,13 @@ public class Hello {
 //        System.out.println("Hello World!");
         Person p = new Person();
         p.hello();
+        int age;
+        age = 22 ;
+        char c = '中';
+        byte b = 1;
+        float f = 12.3f;
+        boolean d = true;
+        String s = new String("eeee");
+        System.out.println(s);
     }
 }
