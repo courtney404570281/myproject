c7b624a94ade3de7587adaafef6e018f194771e5
courtney404570281 <404570281@gapp.fju.edu.tw>
2019-03-02 15:44:35 +0800

    Comparation

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -1,7 +1,15 @@
 fun main(args: Array<String>) {
+
     val h = Human(weight = 66.5f, height = 1.72f)
     println(h.bmi())
     //    println("Hello Kotlin!")
+
+    //練習比較
+    val score = 88;
+    println(score > 80)
+    val c: Char = 'A';
+    println(c.toInt() > 50)
+    println(c > 'a')
 }
 
 class Human (var name: String = "", var weight: Float, var height: Float){
