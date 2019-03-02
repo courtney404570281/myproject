a3eaf58c482c1e99237e2ba450f926aaf178bf59
courtney404570281 <404570281@gapp.fju.edu.tw>
2019-03-02 16:04:43 +0800

    Student

---
+++ src/main/java/student/Student.java
@@ -0,0 +1,14 @@
+package student;
+
+public class Student {
+    String id;
+    String name;
+    int english;
+    int math;
+
+    public Student(String name, int english, int math) {
+        this.name = name;
+        this.english = english;
+        this.math = math;
+    }
+}
