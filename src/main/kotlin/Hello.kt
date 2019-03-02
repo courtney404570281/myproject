5e4f91245a1018e0ef9375721d488f3a0d464d78
courtney404570281 <404570281@gapp.fju.edu.tw>
2019-03-02 15:09:05 +0800

    Constructure

--- src/main/kotlin/Hello.kt
+++ src/main/kotlin/Hello.kt
@@ -1,10 +1,13 @@
 fun main(args: Array<String>) {
-//    println("Hello Kotlin!")
-    val h = Human(66.5f, 1.72f)
+    val h = Human(weight = 66.5f, height = 1.72f)
     println(h.bmi())
+    //    println("Hello Kotlin!")
 }
 
-class Human (var weight: Float, var height: Float){
+class Human (var name: String = "", var weight: Float, var height: Float){
+    init {
+        println("test $weight")
+    }
 
     fun bmi(): Float {
         val bmi = weight / (height*height)
