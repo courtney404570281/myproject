11063ef0d733e1ff99a17dd797327da54e828a40
courtney404570281 <404570281@gapp.fju.edu.tw>
2019-03-03 12:21:01 +0800

    Scanner2

--- src/main/java/student/SchoolRunner.java
+++ src/main/java/student/SchoolRunner.java
@@ -5,12 +5,16 @@ import java.util.Scanner;
 public class SchoolRunner {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        //name
         System.out.println("Please enter student's name: ");
         String name = scanner.next();
+        //english
         System.out.println("Please enter student's english: ");
         int english = scanner.nextInt();
+        //math
         System.out.println("Please enter student's math: ");
         int math = scanner.nextInt();
+
         Student stu = new Student(name, english, math);
         stu.print();
     }
