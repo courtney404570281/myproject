86f138b2250b81ceff7a84381ff20ec144b950f4
courtney404570281 <404570281@gapp.fju.edu.tw>
2019-03-03 11:57:42 +0800

    Scanner

--- src/main/java/student/SchoolRunner.java
+++ src/main/java/student/SchoolRunner.java
@@ -1,8 +1,17 @@
 package student;
 
+import java.util.Scanner;
+
 public class SchoolRunner {
     public static void main(String[] args) {
-        Student stu = new Student("Courtney", 90, 77);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter student's name: ");
+        String name = scanner.next();
+        System.out.println("Please enter student's english: ");
+        int english = scanner.nextInt();
+        System.out.println("Please enter student's math: ");
+        int math = scanner.nextInt();
+        Student stu = new Student(name, english, math);
         stu.print();
     }
 }
