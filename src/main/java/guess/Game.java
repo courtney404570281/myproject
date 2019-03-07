ace4fcb04515f0869d6b4cdc4dd02aaf416c073b
courtney404570281 <404570281@gapp.fju.edu.tw>
2019-03-07 21:48:16 +0800

    Random

--- src/main/java/guess/Game.java
+++ src/main/java/guess/Game.java
@@ -1,9 +1,13 @@
 package guess;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Game {
     public static void main(String[] args) {
+        Random random = new Random();
+        int secret = random.nextInt(10)+1;
+        System.out.println(secret);
         Scanner scanner = new Scanner(System.in);
         int number = 0;
         while (number != -1) {
