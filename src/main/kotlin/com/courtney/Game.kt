59d1139df080c075eab528f79d69e73e549337da
courtney404570281 <404570281@gapp.fju.edu.tw>
2019-03-07 22:03:03 +0800

    Guess Number in Kotlin

--- src/main/kotlin/com/courtney/Game.kt
+++ src/main/kotlin/com/courtney/Game.kt
@@ -1,2 +1,22 @@
 package com.courtney
 
+import java.util.*
+import kotlin.random.Random
+
+fun main() {
+    var secret = Random.nextInt(10)+1
+    println(secret)
+    var number = 0
+    while (number != secret) {
+        print("Please enter a number: ")
+        number = Scanner(System.`in`).nextInt()
+        if (number > secret) {
+            println("Lower")
+        } else if (number < secret) {
+            println("Higher")
+        } else {
+            println("Great! The number is: " + number)
+        }
+    }
+
+}
