fbef290809eba5e5188eef611e07e73728645664
courtney404570281 <404570281@gapp.fju.edu.tw>
2019-03-14 21:56:34 +0800

    Add 1 to 10

--- src/main/java/guess/GameFor.java
+++ src/main/java/guess/GameFor.java
@@ -4,9 +4,12 @@ import java.util.Scanner;
 
 public class GameFor {
     public static void main(String[] args) {
+        int sum = 0;
         for (int i=1; i<=3; i++) {
-            System.out.println(i);
+            //System.out.println(i);
+            sum += 1;
         }
+        System.out.println(sum);
 
         for (int i=1; i<=5; i++) {
             Scanner scanner = new Scanner(System.in);
