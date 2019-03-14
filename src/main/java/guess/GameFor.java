9669d5a47618174e4eb24af2758d5ff81bbc403f
courtney404570281 <404570281@gapp.fju.edu.tw>
2019-03-14 22:08:58 +0800

    Game For Guess Number

--- src/main/java/guess/GameFor.java
+++ src/main/java/guess/GameFor.java
@@ -1,28 +1,38 @@
 package guess;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class GameFor {
     public static void main(String[] args) {
-        int sum = 0;
-        for (int i=1; i<=3; i++) {
-            //System.out.println(i);
-            sum += i;
-        }
-        System.out.println(sum);
-
-        for (int i=5; i>=1; i--) {
-            System.out.println(i);
-        }
-
-        for (int i=1; i<=5; i++) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Please enter number:");
+//        int sum = 0;
+//        for (int i=1; i<=3; i++) {
+//            //System.out.println(i);
+//            sum += i;
+//        }
+//        System.out.println(sum);
+//
+//        for (int i=5; i>=1; i--) {
+//            System.out.println(i);
+//        }
+        int secret = new Random().nextInt(10) + 1;
+        System.out.println(secret);
+        Scanner scanner = new Scanner(System.in);
+        for (int i=1; i<=4; i++) {
+            System.out.print("Please enter number:(" + i + "/4)");
             int number = scanner.nextInt();
             System.out.println("第" + i + "次：" + number);
-            if (number == -1) {
+            if (number > secret) {
+                System.out.println("Lower");
+            } else if (number < secret) {
+                System.out.println("Higher");
+            } else {
+                System.out.println("Great! The number is " + number);
                 break;
             }
+//            if (number == -1) {
+//                break;
+//            }
         }
         System.out.println("結束");
     }
