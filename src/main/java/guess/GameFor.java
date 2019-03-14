4c2bc5499297df7a83fa36269463e0e9320606cb
courtney404570281 <404570281@gapp.fju.edu.tw>
2019-03-14 21:51:36 +0800

    For-Loop

--- src/main/java/guess/GameFor.java
+++ src/main/java/guess/GameFor.java
@@ -1,9 +1,17 @@
 package guess;
 
+import java.util.Scanner;
+
 public class GameFor {
     public static void main(String[] args) {
         for (int i=1; i<=3; i++) {
             System.out.println(i);
+        }
+
+        for (int i=1; i<=5; i++) {
+            Scanner scanner = new Scanner(System.in);
+            int number = scanner.nextInt();
+            System.out.println("第" + i + "次：" + number);
         }
     }
 }
