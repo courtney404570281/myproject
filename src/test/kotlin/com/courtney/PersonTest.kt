40c2442c596fc54ddae88e1103e6e5678643fb99
courtney404570281 <404570281@gapp.fju.edu.tw>
2019-03-14 23:49:25 +0800

    personBmiTest & humanBmiTest in Kotlin

--- src/test/kotlin/com/courtney/PersonTest.kt
+++ src/test/kotlin/com/courtney/PersonTest.kt
@@ -2,11 +2,17 @@ package com.courtney
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import Person
 
 class PersonTest{
     @Test
-    fun bmiTest() {
+    fun humanBmiTest() {
         val human = Human("Courtney", 66.5f, 1.72f);
         Assertions.assertEquals(66.5f / (1.72f*1.72f) ,human.bmi());
+    }
+    @Test
+    fun personBmiTest() {
+        val person = Person(66.5f, 1.72f)
+        Assertions.assertEquals(66.5f / (1.72f*1.72f), person.bmi())
     }
 }
