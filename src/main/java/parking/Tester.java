35390a7765ce12948a22af266d36db6a9a2abfd6
courtney404570281 <404570281@gapp.fju.edu.tw>
2019-03-15 23:30:46 +0800

    Add Date

---
+++ src/main/java/parking/Tester.java
@@ -0,0 +1,10 @@
+package parking;
+
+import java.util.Date;
+
+public class Tester {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date);
+    }
+}
