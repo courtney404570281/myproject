f52932452681bbd7a58907b8f70ca320f648128e
courtney404570281 <404570281@gapp.fju.edu.tw>
2019-03-15 23:37:07 +0800

    Add SimpleDateFormat

--- src/main/java/parking/Tester.java
+++ src/main/java/parking/Tester.java
@@ -1,10 +1,16 @@
 package parking;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Tester {
     public static void main(String[] args) {
         Date date = new Date();
         System.out.println(date);
+
+        System.out.println(date.getTime());
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        System.out.println(sdf.format(date));
     }
 }
