e0b0db376ef696b45ca44c82fab450f85d344617
courtney404570281 <404570281@gapp.fju.edu.tw>
2019-03-18 21:45:30 +0800

    Add Collections

--- src/main/java/many/Tester.java
+++ src/main/java/many/Tester.java
@@ -1,7 +1,17 @@
 package many;
 
+import java.util.ArrayList;
+
 public class Tester {
     public static void main(String[] args) {
+        ArrayList array = new ArrayList();
+        array.add(5);
+        array.add(3);
+        array.add("abc");
+        array.add(true);
+        System.out.println(array);
+        int n1 = (int)array.get(0);
+        String s = (String)array.get(2);
 
 //        arrayTest();
     }
