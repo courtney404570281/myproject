4cd776cff1dfaafa164083be6d72b07f2dc70b1f
courtney404570281 <404570281@gapp.fju.edu.tw>
2019-03-18 22:04:46 +0800

    Add List in Kotlin

---
+++ src/main/kotlin/com/courtney/Many.kt
@@ -0,0 +1,10 @@
+package com.courtney
+
+fun main() {
+    val list = listOf(1, 3, 4, 2, 5)
+    println(list)
+    val scores = listOf(68, 88, 90, 49, 50)
+    for (score in scores) {
+        println(score)
+    }
+}
