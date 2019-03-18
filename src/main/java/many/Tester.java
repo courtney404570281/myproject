c06d828731fc35dc82575d46cd0e3819d38738d5
courtney404570281 <404570281@gapp.fju.edu.tw>
2019-03-19 00:01:16 +0800

    Add hashTest

--- src/main/java/many/Tester.java
+++ src/main/java/many/Tester.java
@@ -1,13 +1,19 @@
 package many;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class Tester {
     public static void main(String[] args) {
-
+          //hashTest
+        HashMap<String, String> stocks = new HashMap<>();
+        stocks.put("2330", "台積電");
+        stocks.put("2317", "鴻海");
+        stocks.put("2330", "TSMC");
+        System.out.println(stocks.get("2330"));
+        System.out.println(stocks);
+        for (String key: stocks.keySet()) {
+            System.out.println(stocks.get(key));
+        }
 //        setTest();
 //        listTest();
 //        arrayTest();
