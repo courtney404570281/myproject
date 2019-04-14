2ee87fecbe99d5c54a2d705c6147ddbf8d37dfbb
courtney404570281 <404570281@gapp.fju.edu.tw>
2019-04-14 21:16:18 +0800

    Add data class in Kotlin

--- src/main/kotlin/com/courtney/Ticket.kt
+++ src/main/kotlin/com/courtney/Ticket.kt
@@ -1,7 +1,22 @@
 package com.courtney
 
+import java.util.*
+
+
 data class Ticket(val origin: Int, val destination: Int, val price: Int)
 
 fun main() {
     val ticket = Ticket(20, 51, 420)
+    val s = "abcde"
+    println(s.validate())
+    println((1..10).random())
+}
+
+fun String.validate(): Boolean {
+    return this.length >= 6
+}
+
+fun IntRange.random(): Int {
+    val r = Random().nextInt(endInclusive-start) + start
+    return r
 }
