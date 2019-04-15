61ceced789ab345fb569f5ba04697a5e3606cdda
courtney404570281 <404570281@gapp.fju.edu.tw>
2019-04-15 22:50:30 +0800

    Add MyThread
    Add MyThread

--- src/main/java/thread/Tester.java
+++ src/main/java/thread/Tester.java
@@ -5,6 +5,27 @@ public class Tester {
         for (int i = 0; i < 10; i++) {
             System.out.println("main:" + i);
         }
+        Thread thread = new Thread(){
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("thread:" + i);
+                }
+            }
+        };
+        thread.start();
+
+        MyThread myThread = new MyThread();
+        myThread.start();
         System.out.println("main end");
+    }
+}
+
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(getName() + ":" + i);
+        }
     }
 }
