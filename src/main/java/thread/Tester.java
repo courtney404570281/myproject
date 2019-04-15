e212e72e540c443e7ae98bc2e75f162c91276333
courtney404570281 <404570281@gapp.fju.edu.tw>
2019-04-15 22:54:42 +0800

    Add Runnable
    Add Runnable

--- src/main/java/thread/Tester.java
+++ src/main/java/thread/Tester.java
@@ -3,20 +3,31 @@ package thread;
 public class Tester {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            System.out.println("main:" + i);
+            System.out.println("main: " + i);
         }
         Thread thread = new Thread(){
             @Override
             public void run() {
                 for (int i = 0; i < 10; i++) {
-                    System.out.println("thread:" + i);
+                    System.out.println("thread: " + i);
                 }
             }
         };
         thread.start();
-
+        // MyThread
         MyThread myThread = new MyThread();
         myThread.start();
+        // Runnable
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("Runnable: " + i);
+                }
+            }
+        };
+        Thread r = new Thread(runnable);
+        r.start();
         System.out.println("main end");
     }
 }
@@ -25,7 +36,7 @@ class MyThread extends Thread {
     @Override
     public void run() {
         for (int i = 0; i < 10; i++) {
-            System.out.println(getName() + ":" + i);
+            System.out.println(getName() + ": " + i);
         }
     }
 }
