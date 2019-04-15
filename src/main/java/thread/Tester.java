f29a8bebedc4451877b8bb115cce81eb0938596f
courtney404570281 <404570281@gapp.fju.edu.tw>
2019-04-15 22:34:41 +0800

    Add Thread
    Add Thread

---
+++ src/main/java/thread/Tester.java
@@ -0,0 +1,10 @@
+package thread;
+
+public class Tester {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("main:" + i);
+        }
+        System.out.println("main end");
+    }
+}
